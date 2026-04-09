23b32fdcef14ee26c8941b0c08d3106512941de6
min_jae083 <93437708+minjae-083@users.noreply.github.com>
2026-04-09 23:25:50 +0900

    날짜 형식 한국어로 변경(merge 전)

--- todo-project/src/utils.py
+++ todo-project/src/utils.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 
 def get_today():
-    """오늘 날짜를 YYYY-MM-DD 형식으로 반환"""
-    return datetime.now().strftime("%Y-%m-%d")
+    """오늘 날짜를 한국어 형식으로 반환"""
+    now = datetime.now()
+    return f"{now.year}년 {now.month}월 {now.day}일"
 
 
 def format_date(date_str):
